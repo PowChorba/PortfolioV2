fcc8f3ebc3fb895d047929b6bb54b5e35d5b1cf2
PowChorba <101152834+PowChorba@users.noreply.github.com>
2023-07-04 16:50:43 -0300

    tecnologies

--- client/src/Components/Skills/SkillsEsp.tsx
+++ client/src/Components/Skills/SkillsEsp.tsx
@@ -4,8 +4,8 @@ import JS from '../../assets/js.png'
 import MONGODB from '../../assets/mongoDb.png'
 import NODE from '../../assets/node.png'
 import REACT from '../../assets/react.png'
-import Redux from '../../assets/redux.png'
-import Sequelize from '../../assets/sequelize.png'
+import TAIL from '../../assets/tailwind.png'
+import NEXT from '../../assets/nextjs.png'
 import SQL from '../../assets/sql.png'
 import TS from '../../assets/ts.png'
 import CSS from '../../assets/css.png'
@@ -39,16 +39,16 @@ export default function SkillsEsp() {
                         <img src={REACT} alt="REACT" width='80px'/>
                     </div>
                     <div>
-                        <img src={Redux} alt="Redux" width='80px'/>
+                        <img src={NEXT} alt="Sequelize" width='80px'/>
+                    </div>
+                    <div>
+                        <img src={TAIL} alt="Redux" width='80px'/>
                     </div>
                     <div>
                         <img src={NODE} alt="NODE" width='80px'/>
                     </div>
                     <div>
                         <img src={MONGODB} alt="MONGODB" width='80px'/>
-                    </div>
-                    <div>
-                        <img src={Sequelize} alt="Sequelize" width='80px'/>
                     </div>
                     <div>
                         <img src={SQL} alt="SQL" width='80px'/>
